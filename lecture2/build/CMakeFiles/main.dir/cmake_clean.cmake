e06dc04ce02a4f204a9e453445ec7ca24380fb6d
wyy <atiredhedonist@gmail.com>
2025-10-03 04:20:49 +0800

    commit homework-lecture2-wuyinyi2350335

---
+++ lecture2/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/homework/main.cpp.o"
+  "CMakeFiles/main.dir/homework/main.cpp.o.d"
+  "CMakeFiles/main.dir/homework/tools.cpp.o"
+  "CMakeFiles/main.dir/homework/tools.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
